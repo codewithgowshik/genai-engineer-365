750dee8dd10afb42b415b838401cb4d164c22c53
Gowshik Subramaniyan <gowshik.uk@gmail.com>
2026-06-09 13:52:27 +0100

    Update config.py

--- src/day-009/src/config.py
+++ src/day-009/src/config.py
@@ -1,1 +1,11 @@
+import os
+from dotenv import load_dotenv
+from google import genai
 
+# Load values from .env
+load_dotenv()
+
+# Create Gemini client
+client = genai.Client(
+    api_key=os.getenv("API_KEY")
+)
