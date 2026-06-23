fa478fa123f5500f20e6ae9123475a65af4217ca
Gowshik Subramaniyan <gowshik.uk@gmail.com>
2026-06-23 21:50:49 +0100

    Delete Projects/llm-powered-cli-assistent/src directory

--- Projects/llm-powered-cli-assistent/src/helper.py
+++
@@ -1,3 +0,0 @@
-# to make a clean prompt and also to avoid unwanted space.
-def clean_prompt(prompt: str):
-    return prompt.strip()
--- Projects/llm-powered-cli-assistent/src/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class requests(BaseModel):
-    prompt : str
-
-class response(BaseModel):
-    answer : str
-    
